f15654742584693e2146e6eb5a5c1d037519c3be
axoking <110609189+axoking@users.noreply.github.com>
2026-03-26 09:43:10 +0100

    Add documentation for linear_example functions

--- src/linear_example.rs
+++ src/linear_example.rs
@@ -1,9 +1,13 @@
-/// Lineare Beispielfunktion. p ist hier der y-Achsenabschnitt
+/*
+This file contains a very simple example for using gradient descent.
+We want to find the best y-intercept for a linear function with a given slope (here 0.64)
+so that it approximately matches a given batch of points
+*/
+
 pub fn linear_example_forward(x: f64, p: f64) -> f64 {
 	x * 0.64 + p
 }
 
-/// Ableitung der linearen Beispielfunktion, abhängig vom Parameter
 pub fn linear_example_derivative(_: f64, _: f64) -> f64 {
 	1.0
 }
