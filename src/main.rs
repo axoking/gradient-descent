8ce27fa761117674bf17941ca9b03571d26339dd
axoking <110609189+axoking@users.noreply.github.com>
2026-03-26 11:23:12 +0100

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use gradient_descent::{*, linear_example::*};
+use gradient_descent::{linear_example::*, *};
 
 fn main() {
     let batch = vec![(1.0, 3.0), (3.0, 4.0), (8.0, 6.0)];
-    
-    let p = gradient_descent(
-        &(linear_slope_forward as ParamFunction),
-        &(linear_slope_derivative as ParamFunction),
-        &batch,
-        0.0,
-        0.3,
-        0.001,
-        1000
-    ).unwrap();
-
-    println!("Best slope for y-intercept 2: {p}");
-    println!("Cost: {}\n", cost(&(linear_slope_forward as ParamFunction), &batch, p));
 
     let p = gradient_descent(
         &(linear_intercept_forward as ParamFunction),
@@ -23,9 +10,28 @@ fn main() {
         0.0,
         0.3,
         0.001,
-        1000
-    ).unwrap();
-
+        5000,
+    )
+    .unwrap();
     println!("Best y-intercept for slope 0.64: {p}");
-    println!("Cost: {}", cost(&(linear_intercept_forward as ParamFunction), &batch, p));
+    println!(
+        "Cost: {}\n",
+        cost(&(linear_intercept_forward as ParamFunction), &batch, p)
+    );
+
+    let p = gradient_descent(
+        &(linear_slope_forward as ParamFunction),
+        &(linear_slope_derivative as ParamFunction),
+        &batch,
+        0.0,
+        0.01, // slope optimiztation needs a smaller learning rate than intercept optimzation, otherwise it fails
+        0.001,
+        5000,
+    )
+    .unwrap();
+    println!("Best slope for y-intercept 2: {p}");
+    println!(
+        "Cost: {}",
+        cost(&(linear_slope_forward as ParamFunction), &batch, p)
+    );
 }
