073ddcef591685bbabed485aea142813dc03c0a0
axoking <110609189+axoking@users.noreply.github.com>
2026-03-26 10:19:15 +0100

    Idk

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use gradient_descent::{*, linear_example::*};
 
 fn main() {
     let batch = vec![(1.0, 3.0), (3.0, 4.0), (8.0, 6.0)];
+    
     let p = gradient_descent(
-        &(linear_example_forward as ParamFunction),
-        &(linear_example_derivative as ParamFunction),
+        &(linear_slope_forward as ParamFunction),
+        &(linear_slope_derivative as ParamFunction),
         &batch,
         0.0,
         0.3,
@@ -12,6 +13,19 @@ fn main() {
         1000
     ).unwrap();
 
-    println!("{p}");
-    println!("Cost: {}", cost(&(linear_example_forward as ParamFunction), &batch, p));
+    println!("Best slope for y-intercept 2: {p}");
+    println!("Cost: {}\n", cost(&(linear_slope_forward as ParamFunction), &batch, p));
+
+    let p = gradient_descent(
+        &(linear_intercept_forward as ParamFunction),
+        &(linear_intercept_derivative as ParamFunction),
+        &batch,
+        0.0,
+        0.3,
+        0.001,
+        1000
+    ).unwrap();
+
+    println!("Best y-intercept for slope 0.64: {p}");
+    println!("Cost: {}", cost(&(linear_intercept_forward as ParamFunction), &batch, p));
 }
