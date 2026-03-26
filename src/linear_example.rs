b090d0ec0ac23bf61bf4cbc34cf9c2aa814e8d4b
axoking <110609189+axoking@users.noreply.github.com>
2026-03-26 10:15:19 +0100

    add slope example

--- src/linear_example.rs
+++ src/linear_example.rs
@@ -4,10 +4,25 @@ We want to find the best y-intercept for a linear function with a given slope (h
 so that it approximately matches a given batch of points
 */
 
-pub fn linear_example_forward(x: f64, p: f64) -> f64 {
+pub fn linear_intercept_forward(x: f64, p: f64) -> f64 {
 	x * 0.64 + p
 }
 
-pub fn linear_example_derivative(_: f64, _: f64) -> f64 {
+pub fn linear_intercept_derivative(x: f64, p: f64) -> f64 {
 	1.0
 }
+
+
+/*
+The same example, but this time the intercept is fixed to 2 and we want to find the best slope
+*/
+
+pub fn linear_slope_forward(x: f64, p: f64) -> f64 {
+    p * x + 2
+}
+
+pub fn linear_slope_derivative(x: f64, p: f64) -> f64 {
+    x
+}
+
+// Next up I should implement optimizing both the slope and y-intercept at once
